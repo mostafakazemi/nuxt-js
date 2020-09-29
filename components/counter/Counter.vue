45edbe9fb8a0f46fd12243501d3ec498f2d41ced
mostafa kazemi <mostafa.kazeminejhad@gmailc.om>
2020-09-29 11:20:59 +0330

    Add eslint

---
+++ components/counter/Counter.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <button @click="add">
+      {{ counter }}
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Counter",
+  data() {
+    return {
+      counter: 0,
+    }
+  },
+  methods: {
+    add() {
+      this.counter++
+    },
+  },
+}
+</script>
+
+<style scoped></style>
